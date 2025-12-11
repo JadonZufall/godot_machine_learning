033adf52e1b54a997f7711c2321d493de6c72d01
Jadon Zufall <jadonzufall@gmail.com>
2025-12-11 03:25:13 -0500

    ML_Agent nodes

---
+++ nodes/ML_Agent.gd
@@ -0,0 +1,3 @@
+@abstract
+@icon("res://Addons/gd_machine_learning/icons/ML_Agent.svg")
+class_name ML_Agent extends Node
---
+++ nodes/ML_Agent3D.gd
@@ -0,0 +1,2 @@
+@icon("res://Addons/gd_machine_learning/icons/ML_Agent3D.svg")
+class_name ML_Agent3D extends ML_Agent
